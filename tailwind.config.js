304fd58f89f59a8b4e2d4e16739214b7c9631b39
minagishl <mvxd568.co@icloud.com>
2025-01-19 23:13:02 +0900

    style: adjust screen size values

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      minHeight: {
+        screen: "100dvh",
+      },
+      height: {
+        screen: "100dvh",
+      },
+      minWidth: {
+        screen: "100dvw",
+      },
+      width: {
+        screen: "100dvw",
+      },
+    },
   },
   plugins: [],
 };
